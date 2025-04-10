8b0c1de5a10328092eab5aee4b6eb5c84480379e
KYSIEMENS <kyle.siemens@gov.bc.ca>
2025-04-10 09:57:54 -0700

    feat: Bias correction scheduled task

--- scripts/src/main_cmc.py
+++ scripts/src/main_cmc.py
@@ -23,50 +23,6 @@ ostore_folder_summary = os.path.join('cmc', 'summary_V2024')
 # TZ = 'America/Toronto'
 TZ = pytz.timezone('America/Vancouver')
 
-# def get_extract_gribs(date_str):
-#     # needs to run at a specific time or the data won't be available
-#     # suspect its before noon
-
-#     # download and process the daily CMC data to local
-#     regional1 = GetGribConfig.GribRegional_1()
-#     if date_str is not None: regional1.datestr = date_str
-#     grib_get_reg1 = GetGrib.GetGrib(config=regional1,
-#                                     dest_folder=dest_folder_gribs,
-#                                     date_str=date_str)
-#     grib_get_reg1.get()
-#     grib_reg1_data = grib_get_reg1.extract()
-
-#     regional2 = GetGribConfig.GribRegional_2()
-#     if date_str is not None: regional2.datestr = date_str
-#     grib_get_reg2 = GetGrib.GetGrib(config=regional2, dest_folder=dest_folder_gribs)
-#     grib_get_reg2.get()
-#     grib_reg2_data = grib_get_reg2.extract()
-
-#     globl = GetGribConfig.GribGlobal_1()
-#     if date_str is not None: globl.datestr = date_str
-#     grib_get_glob1 = GetGrib.GetGrib(config=globl, dest_folder=dest_folder_gribs)
-#     grib_get_glob1.get()
-#     grib_glob1_data = grib_get_glob1.extract()
-
-#     glob2 = GetGribConfig.GribGlobal_2()
-#     if date_str is not None: glob2.datestr = date_str
-#     grib_get_glob2 = GetGrib.GetGrib(config=glob2, dest_folder=dest_folder_gribs)
-#     grib_get_glob2.get()
-#     grib_glob2_data = grib_get_glob2.extract()
-#     # ^^ think about caching the data from this step, then re-read and continue
-#     # to make easier to debug / run
-
-#     # write the grib outputs
-#     coalate = GetGrib.CoalateGribOutput()
-#     coalate.add_dict(grib_reg1_data)
-#     coalate.add_dict(grib_reg2_data)
-#     coalate.add_dict(grib_glob1_data)
-#     coalate.add_dict(grib_glob2_data)
-#     if date_str is None: date_str = datetime.datetime.now(TZ).strftime('%Y%m%d')
-#     dest_folder = os.path.join(dest_folder_summary, date_str)
-#     LOGGER.info(f'dest folder: {dest_folder}')
-#     coalate.output(dest_folder)
-
 def get_extract_gribs(date_str:str, GribConfigList:list, grib_folder:str, summary_folder:str):
     # needs to run at a specific time or the data won't be available
     # suspect its before noon
