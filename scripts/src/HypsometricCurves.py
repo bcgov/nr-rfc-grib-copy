cd5798998021257d3bfe08aebdd6f125f6c1ce2e
KYSIEMENS <kyle.siemens@gov.bc.ca>
2025-08-15 17:22:43 -0700

    feat: Snow Basin hypsometric curves

--- scripts/src/HypsometricCurves.py
+++ scripts/src/HypsometricCurves.py
@@ -8,12 +8,22 @@ import rasterio
 import rasterio.mask
 import geopandas
 
+from arcgis.gis import GIS
+from arcgis.features import FeatureLayer
+#from IPython.display import display
+gis = GIS()
+
+feature_url = "https://services6.arcgis.com/ubm4tcTYICKBpist/ArcGIS/rest/services/Snow_Basins_Indices_View/FeatureServer/0"
+feature_layer = FeatureLayer(feature_url)
+feature_set = feature_layer.query()
+feature_geojson = feature_set.to_geojson
+snow_basins_shp = geopandas.read_file(feature_geojson)
+snow_basins_shp = snow_basins_shp.to_crs("EPSG:3979")
+
 ostore = NRObjStoreUtil.ObjectStoreUtil()
 clever_shp_path = 'data/shape/CLEVER/CLEVER_BASINS.shp'
 clever_shp = geopandas.read_file(clever_shp_path)
-print(clever_shp.crs)
 clever_shp = clever_shp.to_crs("EPSG:3979")
-clever_bounds = clever_shp.bounds
 cog_path = "https://datacube-prod-data-public.s3.ca-central-1.amazonaws.com/store/elevation/mrdem/mrdem-30/mrdem-30-dtm.tif"
 
 def ecdf(a):
@@ -21,26 +31,19 @@ def ecdf(a):
     cusum = np.cumsum(counts)
     return x, np.round(cusum / cusum[-1],5)
 
-# min_x = int(clever_bounds.minx.min()) - 1
-# min_y = int(clever_bounds.miny.min()) - 1
-# max_x = int(clever_bounds.maxx.max()) + 1
-# max_y = int(clever_bounds.maxy.max()) + 1
-for i in range(len(clever_shp.WSDG_ID)):
-        colname = clever_shp.WSDG_ID[i]
+def elevation_ecdf(shapes, names_col, obj_dir):
+       names = shapes.loc[:,names_col]
+       for i in range(len(names)):
+        colname = names[i]
         try:
-                min_x = int(clever_bounds.minx[i]) - 1
-                min_y = int(clever_bounds.miny[i]) - 1
-                max_x = int(clever_bounds.maxx[i]) + 1
-                max_y = int(clever_bounds.maxy[i]) + 1
-
-                output_path = f"data/DEM/mrdem_bc_{clever_shp.WSDG_ID[i]}.tif"
+                output_path = f"data/DEM/mrdem_bc_{names[i]}.tif"
                 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-                vector = clever_shp.iloc[i].geometry
+                vector = shapes.iloc[i].geometry
                 with rasterio.open(cog_path) as src:
                         #window = src.window(min_x, min_y, max_x, max_y)
                         #raster_data = src.read(window=window)
-                        raster_data, out_transform=rasterio.mask.mask(src,[vector],crop=True)
+                        raster_data=rasterio.mask.mask(src,[vector],crop=True)
 
                 raster_data = raster_data.astype(int)
                 elevation_data = raster_data[raster_data>=0]
@@ -49,12 +52,27 @@ for i in range(len(clever_shp.WSDG_ID)):
 
                 ecdf_df = pd.DataFrame(data = cdf, index=x, columns=[colname])
                 fname = f"data/ECDF/{colname}_ECDF.csv"
-                oname = f"Spatial_Files/DEM/ECDF/CLEVER/{colname}_ECDF.csv"
+                oname = os.path.join(obj_dir,f"{colname}_ECDF.csv")
                 os.makedirs(os.path.dirname(fname), exist_ok=True)
                 ecdf_df.to_csv(fname, index_label="Elevation")
                 ostore.put_object(local_path=fname, ostore_path=oname)
         except:
                print(f"{colname} failed")
+
+#names_col = "WSDG_ID"
+#obj_dir = "Spatial_Files/DEM/ECDF/CLEVER"
+#elevation_ecdf(clever_shp, names_col, obj_dir)
+
+names_col = "basinName"
+obj_dir = "Spatial_Files/DEM/ECDF/SnowBasins"
+elevation_ecdf(snow_basins_shp, names_col, obj_dir)
+
+
+
+# min_x = int(clever_bounds.minx.min()) - 1
+# min_y = int(clever_bounds.miny.min()) - 1
+# max_x = int(clever_bounds.maxx.max()) + 1
+# max_y = int(clever_bounds.maxy.max()) + 1
 
 # objpath = "objectstore2.nrs.bcgov/RSImgShare/MRDEM/mrdem-30-dtm-bc.tif"
         # with rasterio.open(objpath) as src:
